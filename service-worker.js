92eb0b5d2013c6b3e9cb5ca87a3e2cd5ad85999e
BrazusB911 <baoB2111915@student.ctu.edu.vn>
2024-10-21 23:24:10 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "number-guessing-game-v1";
-const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/images/icon-192.png", "/images/icon-512.png"];
+const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", function(event) {
   event.waitUntil(
